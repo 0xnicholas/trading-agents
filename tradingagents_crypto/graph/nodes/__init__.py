e0e4c2b16fcb05c544d965bd082d3b328135f6d7
0xnicholas <nicholasli9@qq.com>
2026-04-02 03:38:59 +0800

    graph/nodes/__init__.py: add parallel_analysts exports
    Adds the missing parallel_analysts exports that were present
    in the src/ directory but not in the committed version.

--- tradingagents_crypto/graph/nodes/__init__.py
+++ tradingagents_crypto/graph/nodes/__init__.py
@@ -1,5 +1,15 @@
 """Graph nodes package."""
 from tradingagents_crypto.graph.nodes.meta_agent import meta_agent_node, Task
 from tradingagents_crypto.graph.nodes.analyst_coordinator import analyst_coordinator
+from tradingagents_crypto.graph.nodes.parallel_analysts import (
+    run_analysts_parallel,
+    SyncAnalystAdapter,
+)
 
-__all__ = ["meta_agent_node", "Task", "analyst_coordinator"]
+__all__ = [
+    "meta_agent_node",
+    "Task",
+    "analyst_coordinator",
+    "run_analysts_parallel",
+    "SyncAnalystAdapter",
+]
